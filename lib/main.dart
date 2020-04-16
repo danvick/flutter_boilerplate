ef45a91870cb4164063b88f74add9531f511543c
Danvick Miller <danvickmiller@live.com>
2020-04-16 21:27:28 +0300

    Added flutter_stetho plugin to inspect your HTTP requests in Chromium based browsers

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
+import 'package:debug_mode/debug_mode.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_stetho/flutter_stetho.dart';
 import 'package:flutterreststarter/home_page.dart';
 
 void main() {
+  if(DebugMode.isInDebugMode){
+    Stetho.initialize();
+  }
   runApp(MyApp());
 }
 
