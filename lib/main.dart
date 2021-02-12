d08285ec3e38f3578bf9e8662d9d5146ecf4a5fa
Danvick Miller <danvickmiller@live.com>
2021-02-13 01:45:22 +0300

    Run whole app inside runZonedGuarded - reports all errors to Crashlytics
    Not just FlutterErrors

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,29 @@
-import 'package:firebase_core/firebase_core.dart';
-import 'package:flutter/material.dart';
+import 'dart:async';
+
 import 'package:firebase_analytics/firebase_analytics.dart';
 import 'package:firebase_analytics/observer.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_crashlytics/firebase_crashlytics.dart';
+import 'package:flutter/material.dart';
 
 import 'home_page.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
-  FlutterError.onError = FirebaseCrashlytics.instance.recordFlutterError;
-  runApp(MyApp());
+
+  await runZonedGuarded(() async {
+    FlutterError.onError = FirebaseCrashlytics.instance.recordFlutterError;
+
+    ErrorWidget.builder = (FlutterErrorDetails error) {
+      Zone.current.handleUncaughtError(error.exception, error.stack);
+      return ErrorWidget(error.exception);
+    };
+
+    runApp(MyApp());
+  }, (exception, stackTrace) {
+    FirebaseCrashlytics.instance.recordError(exception, stackTrace);
+  });
 }
 
 class MyApp extends StatelessWidget {
@@ -19,7 +32,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter REST Starter',
+      title: 'Flutter Boilerplate',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
