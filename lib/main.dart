388d4edae85b3016fc64867402e59381f29e9354
Danvick Miller <danvickmiller@live.com>
2022-07-07 15:46:32 +0300

    chore: run all code in main function within runZonedGuarded

--- lib/main.dart
+++ lib/main.dart
@@ -10,16 +10,15 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'app.dart';
 
 void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
-  if (kDebugMode) {
-    await FirebaseCrashlytics.instance.setCrashlyticsCollectionEnabled(false);
-    await FirebasePerformance.instance.setPerformanceCollectionEnabled(false);
-  }
-  await dotenv.load(
-      fileName: kDebugMode ? 'environments/debug.env' : 'environments/.env');
-
   await runZonedGuarded(() async {
+    WidgetsFlutterBinding.ensureInitialized();
+    await Firebase.initializeApp();
+    if (kDebugMode) {
+      await FirebaseCrashlytics.instance.setCrashlyticsCollectionEnabled(false);
+      await FirebasePerformance.instance.setPerformanceCollectionEnabled(false);
+    }
+    await dotenv.load(
+        fileName: kDebugMode ? 'environments/debug.env' : 'environments/.env');
     FlutterError.onError = FirebaseCrashlytics.instance.recordFlutterError;
 
     ErrorWidget.builder = (FlutterErrorDetails error) {
@@ -28,7 +27,7 @@ void main() async {
     };
 
     runApp(const MyApp());
-  }, (exception, stackTrace) {
-    FirebaseCrashlytics.instance.recordError(exception, stackTrace);
-  });
+  },
+      (exception, stackTrace) =>
+          FirebaseCrashlytics.instance.recordError(exception, stackTrace));
 }
