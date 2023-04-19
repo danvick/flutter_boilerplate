7d80c5f9e77ad7bbc86a4b04bac60d78a6b6338d
Danvick Miller <danvickmiller@live.com>
2023-04-19 14:42:40 +0300

    feat: upgrade packages

--- lib/utils/http_interceptors/error_interceptor.dart
+++ lib/utils/http_interceptors/error_interceptor.dart
@@ -2,51 +2,68 @@ import 'package:dio/dio.dart';
 
 class ErrorInterceptor extends Interceptor {
   @override
-  void onError(DioError err, ErrorInterceptorHandler handler) {
+  void onError(DioError err, ErrorInterceptorHandler handler) async {
+    DioError dioError = err;
     switch (err.type) {
       case DioErrorType.cancel:
-        err.error = 'Request to API server was cancelled';
+        dioError = err.copyWith(error: 'Request to API server was cancelled');
         break;
-      case DioErrorType.connectTimeout:
-        err.error = 'Connection to API server timed out';
+      case DioErrorType.connectionTimeout:
+        dioError = err.copyWith(error: 'Connection to API server timed out');
         break;
       case DioErrorType.receiveTimeout:
-        err.error = 'Receive timeout in connection with API server';
+        dioError = err.copyWith(
+          error: 'Receive timeout in connection with API server',
+        );
         break;
       case DioErrorType.sendTimeout:
-        err.error = 'Send timeout in connection with API server';
+        dioError = err.copyWith(
+          error: 'Send timeout in connection with API server',
+        );
         break;
-      case DioErrorType.response:
+      case DioErrorType.badResponse:
         if (err.response!.data != null) {
           if (err.response!.data is String) {
-            err.error = '${err.response!.statusCode}: ${err.response!.data}';
+            dioError = err.copyWith(
+                error: '${err.response!.statusCode}: ${err.response!.data}');
+          } else if (err.response!.data is List) {
+            dioError = err.copyWith(
+                error: err.response!.data
+                    .map((el) => '${el['message']}\n')
+                    .toList()
+                    .join());
           } else {
-            err.error = err.response!.data['message'];
+            dioError =
+                err.copyWith(error: err.response!.data['message'].toString());
           }
           if (err.response!.statusCode == 404 && err.response!.data is String) {
-            err.error = '${err.response!.statusCode} Page not found.';
+            dioError = err.copyWith(
+                error: '${err.response!.statusCode} Resource not found.');
           }
           if (err.response!.statusCode == 500 && err.response!.data is String) {
-            err.error = '${err.response!.statusCode} Internal server error.';
-          }
-
-          if (err.response!.statusCode == 401) {
-            err.error = 'Unauthenticated';
-          }
-          if (err.response!.statusCode == 403) {
-            err.error = 'Unauthorized';
+            dioError = err.copyWith(
+                error: '${err.response!.statusCode} Internal server error.');
           }
         } else {
-          err.error =
-              'Received invalid status code: ${err.response!.statusCode}';
+          dioError = err.copyWith(
+            error: 'Received invalid status code: ${err.response!.statusCode}',
+          );
         }
         break;
-
-      case DioErrorType.other:
-        err.error =
-            'Connection to API server failed due to internet connection';
+      case DioErrorType.badCertificate:
+        dioError = err.copyWith(error: 'Certificate validation failed');
+        break;
+      case DioErrorType.connectionError:
+        dioError = err.copyWith(
+          error: 'Connection to API server failed due to internet connection',
+        );
+        break;
+      case DioErrorType.unknown:
+        dioError = err.copyWith(
+          error: 'Connection to API server failed due to unknown error',
+        );
         break;
     }
-    handler.next(err);
+    return handler.next(dioError);
   }
 }
